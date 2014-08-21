5e52852f8a9b3bbbaea20f3532eb3ee7e42343e0
andrea parodi <andrea.parodi@ebansoftware.net>
2014-08-21 12:39:13 +0200

    cli improved

--- engine/lib/jorc.js
+++ engine/lib/jorc.js
@@ -1,14 +1,36 @@
-#!/home/parroit/.nvm/v0.11.13/bin/node --harmony
-
+#! /usr/bin/env node
 'use strict';
 
-var program = require('commander');
+if (isHarmony()) {
+    startEngine();
+} else {
+    harmonize();
+}
 
-program
-  .version('0.1.0')
-  .option('-f, --folder <path>', 'path to folder containing jor.yml file')
-  .parse(process.argv);
 
-var engine = require('./jor-engine.js');
+function startEngine(){
+    var program = require('commander');
 
-engine.start(program.folder || process.cwd());
+    program
+      .version('0.1.0')
+      .option('-f, --folder <path>', 'path to folder containing jor.yml file')
+      .parse(process.argv);
+
+    var engine = require('./jor-engine.js');
+
+    engine.start(program.folder || process.cwd());
+
+}
+
+
+
+function isHarmony(){
+    return 'function' === typeof Map;
+}
+
+function harmonize(){
+    var spawn = require('child_process').spawn;
+    var args = [ __filename].concat(process.argv.slice(2));
+
+    spawn(process.argv[0], ['--harmony'].concat(args), {stdio: [0,1,2]});
+}
